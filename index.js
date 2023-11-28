28b57c1a060bb674c3ccbeb31a88d3930ec7491e
CaioAlmeida09 <Caio09092001@gmail.com>
2023-11-28 20:28:12 -0300

    ...

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ import { products } from "./products";
 
 const app = express();
 const port = process.env.PORT || 3333;
-
 app.get("/", (req, res) => {
   return res.json(products);
+});
+
+app.get("/teste", (res) => {
+  return res.send("Rota teste");
 });
 
 app.listen(3333, () => {
